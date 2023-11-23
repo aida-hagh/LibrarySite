d19fbaf6fa20e70d2f7bb8f0f6910ff48908595f
aida <aida1992.h@gmail.com>
2023-11-24 00:00:18 +0330

    Add project files.

---
+++ LibrarySite/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySite.Areas.AdminArea.Controllers
+{
+
+    [Area("AdminArea")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibrarySite/Areas/UserArea/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySite.Areas.UserArea.Controllers
+{
+    [Area("UserArea")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
